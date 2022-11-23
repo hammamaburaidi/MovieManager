1c661bf828f58ea57b0f47b96a433aec66e1eb98
Hammam Aburaidi <89531299+hammamaburaidi@users.noreply.github.com>
2022-11-23 18:08:42 +0300

    I started the project and add fonts and some assets

---
+++ movieManager/movieManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movieManager
+//
+//  Created by Hammam Aburaidi on 23.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
